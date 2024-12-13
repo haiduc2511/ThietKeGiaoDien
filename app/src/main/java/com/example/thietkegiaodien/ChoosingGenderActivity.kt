4c17ea95ad50f71e4e8ca9dfe74339fcfdbf4853
ADMIN <nguyenhuuhaiduc.2003@gmail.com>
2024-12-13 15:15:24 +0700

    add: ic_turn_left & right, trying to use imageButton to replace fab

--- app/src/main/java/com/example/thietkegiaodien/ChoosingGenderActivity.kt
+++ app/src/main/java/com/example/thietkegiaodien/ChoosingGenderActivity.kt
@@ -1,5 +1,6 @@
 package com.example.thietkegiaodien
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.Button
@@ -9,6 +10,7 @@ import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ChoosingGenderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,20 +32,29 @@ class ChoosingGenderActivity : AppCompatActivity() {
         val manButton: Button = findViewById(R.id.bt_man)
         val womanButton: Button = findViewById(R.id.bt_woman)
         val rootLayout: ConstraintLayout = findViewById(R.id.main)
+        val fabTurnLeft: FloatingActionButton = findViewById(R.id.fab_turn_left)
+        val fabTurnRight: FloatingActionButton = findViewById(R.id.fab_turn_right)
 
         manButton.isSelected = true
         womanButton.isSelected = false
+
+        fabTurnLeft.setBackgroundResource(R.drawable.button_man)
+        fabTurnRight.setBackgroundResource(R.drawable.button_man)
 
         manButton.setOnClickListener {
             rootLayout.setBackgroundResource(R.drawable.choose_gender_man_background)
             manButton.isSelected = true
             womanButton.isSelected = false
+            manButton.setTextColor(Color.WHITE)
+            womanButton.setTextColor(Color.BLACK)
         }
 
         womanButton.setOnClickListener {
             rootLayout.setBackgroundResource(R.drawable.choose_gender_woman_background)
             womanButton.isSelected = true
             manButton.isSelected = false
+            womanButton.setTextColor(Color.WHITE)
+            manButton.setTextColor(Color.BLACK)
         }
 
     }
