f6abb6fce69df71909e4c6dd98985970339f66fc
ADMIN <nguyenhuuhaiduc.2003@gmail.com>
2024-12-20 10:33:43 +0700

    add: ChoosingWeightActivity background add: Style questionCardview, unitButtonStyle and textViewStyle for re-use purpose

--- app/src/main/java/com/example/thietkegiaodien/ChoosingWeightActivity.kt
+++ app/src/main/java/com/example/thietkegiaodien/ChoosingWeightActivity.kt
@@ -1,8 +1,11 @@
 package com.example.thietkegiaodien
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -10,5 +13,13 @@ class ChoosingWeightActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choosing_weight)
+        val rootLayout: ConstraintLayout = findViewById(R.id.main)
+        val sharedPrefs = SharedPreferenceManager(this)
+        if (sharedPrefs.getString("gender").equals("man")) {
+            rootLayout.setBackgroundResource(R.drawable.choose_gender_man_background)
+        } else {
+            rootLayout.setBackgroundResource(R.drawable.choose_gender_woman_background)
+        }
+
     }
 }
