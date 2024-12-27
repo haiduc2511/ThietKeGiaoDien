e35cac8dc8756621e1c36a422771e5adb0ec9368
ADMIN <nguyenhuuhaiduc.2003@gmail.com>
2024-12-27 15:02:48 +0700

    add: TrackDietActivity

---
+++ app/src/main/java/com/example/thietkegiaodien/activitycontinue/TrackDietActivity.kt
@@ -0,0 +1,16 @@
+package com.example.thietkegiaodien.activitycontinue
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.thietkegiaodien.R
+
+class TrackDietActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_track_diet)
+
+    }
+}
