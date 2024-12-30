4c1cee8df53a23f2775777a87bc5494c5503600d
ADMIN <nguyenhuuhaiduc.2003@gmail.com>
2024-12-30 14:21:40 +0700

    add: implement TrackDietActivity

--- app/src/main/java/com/example/thietkegiaodien/activitycontinue/TrackDietActivity.kt
+++ app/src/main/java/com/example/thietkegiaodien/activitycontinue/TrackDietActivity.kt
@@ -1,16 +1,42 @@
 package com.example.thietkegiaodien.activitycontinue
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.thietkegiaodien.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.thietkegiaodien.adapter.FoodAdapter
+import com.example.thietkegiaodien.model.FoodModel
+import com.example.thietkegiaodien.utils.SharedPreferenceManager
 
 class TrackDietActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_track_diet)
+        val rootLayout: ConstraintLayout = findViewById(R.id.main)
+        val sharedPrefs = SharedPreferenceManager(this)
+        if (sharedPrefs.getString("gender").equals("man")) {
+            rootLayout.setBackgroundResource(R.drawable.choose_gender_man_background)
+        } else {
+            rootLayout.setBackgroundResource(R.drawable.choose_gender_woman_background)
+        }
+
+        val foodList = listOf(
+            FoodModel("Apple", "1 unit", 95),
+            FoodModel("Banana", "1 unit", 105),
+            FoodModel("Pizza", "1 slice", 285),
+            FoodModel("Salad", "1 bowl", 150)
+        )
+
+        // Set up RecyclerView
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = FoodAdapter(foodList)
 
     }
 }
