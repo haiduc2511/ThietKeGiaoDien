adc883ccf018938ea5286e62e1d1887eec7fb230
ADMIN <nguyenhuuhaiduc.2003@gmail.com>
2025-01-06 10:24:54 +0700

    add: AddDishSuccesfullyActivity

--- app/src/main/java/com/example/thietkegiaodien/activitycontinue/AddDishSuccesfullyActivity.kt
+++ app/src/main/java/com/example/thietkegiaodien/activitycontinue/AddDishSuccesfullyActivity.kt
@@ -11,28 +11,24 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.thietkegiaodien.R
 import com.example.thietkegiaodien.utils.SharedPreferenceManager
 
-class ChooseWeightActivity2 : AppCompatActivity() {
+class AddDishSuccesfullyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_weight2)
+        setContentView(R.layout.activity_add_dish_succesfully)
         val rootLayout: ConstraintLayout = findViewById(R.id.main)
         val sharedPrefs = SharedPreferenceManager(this)
-        val fabTurnLeft: ImageButton = findViewById(R.id.fab_turn_left)
         val fabHome: ImageButton = findViewById(R.id.fab_home)
-        val fabTurnRight: ImageButton = findViewById(R.id.fab_turn_right)
         if (sharedPrefs.getString("gender").equals("man")) {
             rootLayout.setBackgroundResource(R.drawable.choose_gender_man_background)
-            fabTurnLeft.setBackgroundResource(R.drawable.button_turn_man)
             fabHome.setBackgroundResource(R.drawable.button_turn_man)
-            fabTurnRight.setBackgroundResource(R.drawable.button_turn_man)
         } else {
             rootLayout.setBackgroundResource(R.drawable.choose_gender_woman_background)
-            fabTurnLeft.setBackgroundResource(R.drawable.button_turn_woman)
             fabHome.setBackgroundResource(R.drawable.button_turn_woman)
-            fabTurnRight.setBackgroundResource(R.drawable.button_turn_woman)
         }
-        fabTurnRight.setOnClickListener {
-            Intent(this@ChooseWeightActivity2, AddDishSuccesfullyActivity::class.java).apply {
+
+        fabHome.setOnClickListener {
+            Intent(this, FoodActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
                 startActivity(this)
             }
         }
