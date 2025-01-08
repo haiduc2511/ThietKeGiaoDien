c070e1e17dfd8886098dd5f00413071105a458f5
ADMIN <nguyenhuuhaiduc.2003@gmail.com>
2025-01-08 13:49:26 +0700

    fix: tab layout replaced by 2 constraintLayout

--- app/src/main/java/com/example/thietkegiaodien/activitycontinue/TodayPlanActivity.kt
+++ app/src/main/java/com/example/thietkegiaodien/activitycontinue/TodayPlanActivity.kt
@@ -7,6 +7,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -26,14 +27,36 @@ class TodayPlanActivity : AppCompatActivity() {
         val rootLayout: LinearLayout = findViewById(R.id.main)
         val sharedPrefs = SharedPreferenceManager(this)
         val viewPager: ViewPager2 = findViewById(R.id.viewPager)
-        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        val clTodayPlan: ConstraintLayout = findViewById(R.id.tab_layout_today_plan)
+        val clGeneralReport: ConstraintLayout = findViewById(R.id.tab_layout_general_report)
+        val tvTodayPlan: TextView = findViewById(R.id.tv_title_today_plan)
+        val tvGeneralReport: TextView = findViewById(R.id.tv_title_general_report)
+        tvTodayPlan.setTextColor(ContextCompat.getColor(this@TodayPlanActivity, R.color.white))
+        tvGeneralReport.setTextColor(ContextCompat.getColor(this@TodayPlanActivity, R.color.black))
 
         if (sharedPrefs.getString("gender").equals("man")) {
             rootLayout.setBackgroundResource(R.drawable.choose_gender_man_background)
-//            tabLayout.setBackgroundResource(R.drawable.custom_tablayout_man_selector)
+            clTodayPlan.setBackgroundResource(R.drawable.custom_tablayout_man_selector)
+            clGeneralReport.setBackgroundResource(R.drawable.custom_tablayout_man_selector)
         } else {
             rootLayout.setBackgroundResource(R.drawable.choose_gender_woman_background)
-//            tabLayout.setBackgroundResource(R.drawable.custom_tablayout_woman_selector)
+            clTodayPlan.setBackgroundResource(R.drawable.custom_tablayout_woman_selector)
+            clGeneralReport.setBackgroundResource(R.drawable.custom_tablayout_woman_selector)
+        }
+        clTodayPlan.isSelected = true
+        clGeneralReport.isSelected = false
+
+        clTodayPlan.setOnClickListener {
+            clTodayPlan.isSelected = true
+            clGeneralReport.isSelected = false
+            tvTodayPlan.setTextColor(ContextCompat.getColor(this@TodayPlanActivity, R.color.white))
+            tvGeneralReport.setTextColor(ContextCompat.getColor(this@TodayPlanActivity, R.color.black))
+        }
+        clGeneralReport.setOnClickListener {
+            clTodayPlan.isSelected = false
+            clGeneralReport.isSelected = true
+            tvTodayPlan.setTextColor(ContextCompat.getColor(this@TodayPlanActivity, R.color.black))
+            tvGeneralReport.setTextColor(ContextCompat.getColor(this@TodayPlanActivity, R.color.white))
         }
 
         val fragments = listOf(
@@ -49,68 +72,7 @@ class TodayPlanActivity : AppCompatActivity() {
         val adapter = ViewPagerAdapter(this, fragments)
         viewPager.adapter = adapter
 
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.customView = createCustomTab(titles[position], position)
 
-        }.attach()
-
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                var customTabLayout = tab.customView!!.findViewById<View>(R.id.main_custom_tab_layout)
-                customTabLayout.isSelected = true
-                tab.customView!!.findViewById<TextView>(R.id.tabTitle).apply {
-                    this.setTextColor(ContextCompat.getColor(this@TodayPlanActivity, R.color.white))
-                    this.text = titles[tab.position]
-                }
-
-                when (tab.position) {
-                    0 -> {
-                        Toast.makeText(this@TodayPlanActivity, "Plan for today clicked!", Toast.LENGTH_SHORT).show()
-                    }
-                    1 -> {
-                        Toast.makeText(this@TodayPlanActivity, "General report clicked!", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {
-                var customTabLayout = tab.customView!!.findViewById<View>(R.id.main_custom_tab_layout)
-                customTabLayout.isSelected = false
-                tab.customView!!.findViewById<TextView>(R.id.tabTitle).apply {
-                    this.setTextColor(ContextCompat.getColor(this@TodayPlanActivity, R.color.black))
-                }
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab) {
-                // Handle tab re-clicked
-                Toast.makeText(this@TodayPlanActivity, "Tab re-clicked: ${tab.text}", Toast.LENGTH_SHORT).show()
-            }
-        })
-
-    }
-    private fun createCustomTab(title: String, position: Int): View {
-        val tabView = LayoutInflater.from(this).inflate(R.layout.custom_tab, null)
-        if (SharedPreferenceManager(this).getString("gender").equals("man")) {
-            tabView.setBackgroundResource(R.drawable.custom_tablayout_man_selector)
-        } else {
-            tabView.setBackgroundResource(R.drawable.custom_tablayout_woman_selector)
-        }
-        when (position) {
-            0 -> {
-                tabView.findViewById<TextView>(R.id.tabTitle).apply {
-                    this.setTextColor(ContextCompat.getColor(this@TodayPlanActivity, R.color.white))
-                    this.text = title
-                }
-            }
-            1 -> {
-                tabView.findViewById<TextView>(R.id.tabTitle).apply {
-                    this.setTextColor(ContextCompat.getColor(this@TodayPlanActivity, R.color.black))
-                    this.text = title
-                }
-            }
-        }
-
-        return tabView
     }
 
     class ViewPagerAdapter(
